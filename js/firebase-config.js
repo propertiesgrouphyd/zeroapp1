970503297dec9083ed441107268e42375a70f34e
propertiesgrouphyd <propertiesgrouphyd@gmail.com>
2026-06-06 23:41:39 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,58 +1,34 @@
-// ======================================================
-// QUANTUMFEED
-// FIREBASE V10
-// ======================================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-
-import { getFirestore }
-from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-
-// ======================================================
-// CONFIG
-// ======================================================
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey:
-  "AIzaSyAt4d11ASECig6a2ByaLg_716AuLh6HPdc",
-
-  authDomain:
-  "quantumfeed-e9b1d.firebaseapp.com",
-
-  projectId:
-  "quantumfeed-e9b1d",
-
-  storageBucket:
-  "quantumfeed-e9b1d.firebasestorage.app",
-
-  messagingSenderId:
-  "304072670051",
-
-  appId:
-  "1:304072670051:web:f233d7622165781f54b156"
-
+  apiKey: "AIzaSyAt4d11ASECig6a2ByaLg_716AuLh6HPdc",
+  authDomain: "quantumfeed-e9b1d.firebaseapp.com",
+  projectId: "quantumfeed-e9b1d",
+  storageBucket: "quantumfeed-e9b1d.firebasestorage.app",
+  messagingSenderId: "304072670051",
+  appId: "1:304072670051:web:f233d7622165781f54b156"
 };
 
-// ======================================================
-// INIT
-// ======================================================
+const app = initializeApp(firebaseConfig);
 
-const app =
-initializeApp(
-  firebaseConfig
-);
-
-const db =
-getFirestore(
-  app
-);
-
-// ======================================================
-// EXPORT
-// ======================================================
+const db = getFirestore(app);
 
 export {
-  db
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit
 };
