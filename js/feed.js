8bfadf8cc70bf07acf7bf9b871f913b4c70e5e0b
propertiesgrouphyd <propertiesgrouphyd@gmail.com>
2026-06-06 23:46:49 +0530

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -423,3 +423,23 @@ uploadModal.classList.add(
 // ======================================================
 
 loadVideos();
+
+// ======================================================
+// SERVICE WORKER
+// ======================================================
+
+if(
+"serviceWorker" in navigator
+){
+
+window.addEventListener(
+"load",
+()=>{
+
+navigator.serviceWorker
+.register("/sw.js")
+.catch(console.error);
+
+});
+
+}
