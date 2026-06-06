06e40eac67c9bc3d4ccd8db5987b129535c0d2bf
propertiesgrouphyd <propertiesgrouphyd@gmail.com>
2026-06-07 04:01:11 +0530

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -200,7 +200,8 @@ reel.innerHTML = `
 <div class="video-placeholder">
 
 <img
-src="https://i.ytimg.com/vi/${videoId}/hqdefault.jpg"
+src="https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg"
+onerror="this.src='https://i.ytimg.com/vi/${videoId}/hqdefault.jpg'"
 loading="lazy">
 
 <button class="play-btn">
@@ -368,7 +369,7 @@ reel.prepend(
 iframe
 );
 
-
+iframe.style.pointerEvents = "none";
 
 
 
